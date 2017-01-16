34197c91451f85d8d2e8a23533ef0aab25581a5f
alessandro <alessandro@siel.it>
2017-01-16 16:54:00 +0100

    first commit

---
+++ src/main/java/bdmp/project/App.java
@@ -0,0 +1,13 @@
+package bdmp.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
